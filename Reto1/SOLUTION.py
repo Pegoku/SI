a9c2b0eb722dcf05763bb47b65b3a1b9115897ad
Pegoku <pegoku08@gmail.com>
2025-09-28 13:52:46 +0200

    commit

---
+++ Reto1/SOLUTION.py
@@ -0,0 +1,18 @@
+import ast
+
+width = 929
+with open('data', 'r') as file:
+    a = file.read()
+    ab = ast.literal_eval(a)
+#print(b)
+#print(type(b))
+
+loopIt = 0
+
+while True:
+    for i in range(width):
+        r, g, b = ab[i+width*loopIt]
+        print(f"\033[48;2;{r};{g};{b}m.\033[0m", end='')
+        #print("R:", r, "G:", g, "B:", b, end='')
+    print(loopIt)
+    loopIt += 1
