aa05f09a8a251eda3471ab9b87bf4eb7c01697c1
Pegoku <pegoku08@gmail.com>
2026-01-28 12:17:50 +0100

    script recreation

---
+++ Reto11/main.py
@@ -0,0 +1,43 @@
+number = int(input("Enter a number:"))
+
+a=200
+b=0
+
+a=a/2
+a=a-7
+a+=1
+
+b+=1
+b=b*70
+b=b+12
+b-=1
+
+m="FLAG:"
+q="MUcH"
+w="Le$$"
+e="shHsH"
+r="ReAd"
+t="wAtcH"
+y="NoT"
+u="tV"
+i="f1Lm"
+o="B0oK"
+p="d0"
+s="pWd"
+d="C0mPuTeR"
+f="12345"
+g="54321"
+h="AbCdE"
+j="..."
+
+if number<a:
+  if number%2 == 0:
+    if number == 88:
+      print (h+f+d+u+e)
+  else:
+    if number == 87:
+      print(m+r+w+u+e)
+
+
+    
+    
