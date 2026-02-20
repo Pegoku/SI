de097d93a39d37071d567af3d1d8034f32cef458
Pegoku <pegoku08@gmail.com>
2026-02-20 14:42:06 +0100

    remove func1

--- Reto15/index.js
+++ Reto15/index.js
@@ -16,33 +16,34 @@ var valueArr = [
   "value",
   "5eMgJYX",
 ];
-var value1 = function (p1, unusedParam) {
-//   p1 -= 330;
-  var result = valueArr[p1];
-  return result;
-};
-(function (_0x1d7e2f, _0x1e9b0f) {
-  while (!![]) {
+// function valueArr (pa1, unusedParam) {
+// //   p1 -= 330;
+//   var result = valueArr[pa1];
+//   return result;
+// };
+function func2 (pb1, pb2) {
+  while (true) {
     try {
-      var _0x20dbc3 =
-        parseInt(value1(4)) * parseInt(value1(12)) +
-        -parseInt(value1(9)) +
-        -parseInt(value1(2)) +
-        -parseInt(value1(7)) +
-        parseInt(value1(8)) * parseInt(value1(14)) +
-        parseInt(value1(13)) +
-        parseInt(value1(0));
-      if (_0x20dbc3 === _0x1e9b0f) break;
-      else _0x1d7e2f["push"](_0x1d7e2f["shift"]());
+      var _calculatedValue =
+        parseInt(valueArr[4]) * parseInt(valueArr[12]) +
+        -parseInt(valueArr[9]) +
+        -parseInt(valueArr[2]) +
+        -parseInt(valueArr[7]) +
+        parseInt(valueArr[8]) * parseInt(valueArr[14]) +
+        parseInt(valueArr[13]) +
+        parseInt(valueArr[0]);
+      if (_calculatedValue === pb2) break;
+      else pb1["push"](pb1["shift"]());
     } catch (_0x192125) {
-      _0x1d7e2f["push"](_0x1d7e2f["shift"]());
+      pb1["push"](pb1["shift"]());
     }
   }
-})(valueArr, 891185);
+}
+func2(valueArr, 891185);
 function verify() {
-  password = document[value1(15)](value1(10))[value1(3)];
-  var _0x1ccb0d = value1(11);
-  password == value1(1) + _0x1ccb0d
-    ? alert(value1(6))
-    : alert(value1(5));
+  password = document[valueArr[15]](valueArr[10])[valueArr[3]];
+  var _0x1ccb0d = valueArr[11];
+  password == valueArr[1] + _0x1ccb0d
+    ? alert(valueArr[6])
+    : alert(valueArr[5]);
 }
